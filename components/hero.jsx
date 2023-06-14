73264aba493154b468159c400bc6c844a5f36c55
matiaslapolla <matias.lapolla.1@gmail.com>
2023-06-13 21:47:45 -0300

    changed some stuff

--- components/hero.jsx
+++ components/hero.jsx
@@ -2,9 +2,14 @@ import React from "react";
 
 const Hero = () => {
 	return (
-		<>
-			<h1 className="text-3xl">Hero</h1>
-		</>
+		<section className="bg-gray-900 text-white text-center content-center justify-center items-center self-center py-12">
+			<div className="w-screen items-center justify-center content-center flex">
+				<h1 className="border w-1/2 self-center content-center justify-center text-center">
+					Hero
+				</h1>
+			</div>
+			<h1 className="border justify-center text-center">Hero</h1>
+		</section>
 	);
 };
 
--- components/navBar.jsx
+++ components/navBar.jsx
@@ -1,31 +1,59 @@
 import React from "react";
-import { Inter } from "@next/font/google";
 import Link from "next/link";
 
-const inter = Inter({ subsets: ["latin"] });
-
-const NavBar = () => {
+const Navbar = () => {
 	return (
-		<>
-			<div>
-				<ul className="flex border gap-2 justify-between">
-					<li>
-						<Link className="font-bold" href={"/index"}>
-							Inicio
+		<nav className="bg-gray-800">
+			<div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+				<div className="flex items-center justify-between h-16">
+					<div className="flex items-center">
+						<Link
+							href={"/"}
+							className="text-lg font-medium leading-5 text-white"
+						>
+							servifinder
 						</Link>
-					</li>
-					<div className="flex gap-2">
-						<li>
-							<Link href={"/register"}>Registrarse</Link>
-						</li>
-						<li>
-							<Link href={"/signin"}>Iniciar sesion</Link>
-						</li>
 					</div>
-				</ul>
+					<div className="sm:hidden">
+						<button
+							type="button"
+							className="text-gray-500 hover:text-white focus:text-white focus:outline-none"
+						>
+							<svg
+								className="h-6 w-6"
+								stroke="currentColor"
+								fill="none"
+								viewBox="0 0 24 24"
+							>
+								<path
+									strokeLinecap="round"
+									strokeLinejoin="round"
+									strokeWidth="2"
+									d="M4 6h16M4 12h16M4 18h16"
+								/>
+							</svg>
+						</button>
+					</div>
+					<div className="hidden sm:flex sm:items-center sm:ml-6">
+						<nav>
+							<Link
+								href="/about"
+								className="text-base font-medium leading-5 text-white hover:text-gray-300 focus:outline-none focus:underline transition duration-150 ease-in-out"
+							>
+								About
+							</Link>
+							<Link
+								href="/contact"
+								className="text-base font-medium leading-5 text-white ml-8 hover:text-gray-300 focus:outline-none focus:underline transition duration-150 ease-in-out"
+							>
+								Contact
+							</Link>
+						</nav>
+					</div>
+				</div>
 			</div>
-		</>
+		</nav>
 	);
 };
 
-export default NavBar;
+export default Navbar;
